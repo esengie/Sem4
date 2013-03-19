31abe30f0814433597d612ced5483e06858dbff5
esengie <2esengie2@gmail.com>
2013-03-20 02:13:38 +0400

    your ad still could be here

--- 4/No5.hs
+++ 4/No5.hs
@@ -1,37 +1,42 @@
 module Main where
 
 import GHC.IO
-
+import Data.Char
    
-addTo :: (Ord a) => [a] -> a -> [a]
+addTo :: [Int] -> Int -> [Int]
 addTo [] var = [var]
 addTo x var | head(x) == var = x
-	    | head(x) > var = (var:x)
+	    | head(x) > var =  (var:x)
 	    | otherwise = (head(x) : addTo (tail x) var)
 	    
 	    				   
-removeFrom :: (Ord a) => [a] -> a -> [a]
-removeFrom [] var = []
+removeFrom :: [Int] -> Int -> [Int]
+removeFrom [] var = [] 
 removeFrom x var | head(x) == var = tail(x)
 		 | otherwise = (head(x): removeFrom (tail x) var)
    
   
 
-doLoop = do
-	    let listt = []
+doLoop listt = do
+	    
 	    putStrLn "Enter a command \n0 - exit \n1 - add value to sorted list \n2 - remove value from list \n3 - print list:"
 	    command <- getLine
 	    case command of
 			    '0': _ ->   return ()
-			    '1': var -> do  
-					    print  (let listt = (addTo listt var))
-					      doLoop
-			    '2': var -> do 
-					    print (let listt = (removeFrom listt var))
-					    doLoop
-			    '3': _ ->   do print listt
-					      doLoop
+			    '1': _ -> do  var <- getLine
+					  listt <- return (addTo listt (digitToInt $ head $ tail (show var))):: IO[Int]
+					  print  listt
+					  (doLoop listt)
+			    '2': _ -> do var <- getLine
+					 listt <- return (removeFrom listt (digitToInt $ head $ tail  (show var))) :: IO[Int]
+					 print listt
+					 (doLoop listt)
+			    '3': _ ->   do 
+					   print listt
+					   (doLoop listt)
+			     
                                
 main = do
-	  hSetBuffering stdin LineBuffering
-	  doLoop
+	 -- hSetBuffering stdin LineBuffering
+	 listt <- return [] :: IO [Int]
+	 doLoop listt
