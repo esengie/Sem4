04c2123c6bc36b3254eb73cfa653f734e37a7fdb
esengie <2esengie2@gmail.com>
2013-04-10 12:46:38 +0400

    ugly

--- midterm1_reloaded/No3.hs
+++ midterm1_reloaded/No3.hs
@@ -4,7 +4,8 @@ stars = '*':stars
 
 does n = does' n (n - 1)
 does' n k
-	  | k >= 0 = ir:ini ++ [im]		       
+	  | k > 0 = ir:ini ++ [im]		       
+	  | k == 0 = [take (n + n - 1) stars]
 	  | otherwise = []
 		  where ir = take k spaces ++ take (n + n - 1 - 2*k) stars ++ take k spaces		      
 			ini = does' n (k-1)
