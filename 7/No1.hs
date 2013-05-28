e65530cdf956335d70fe9cfe2988214c8d3feb3d
esengie <2esengie2@gmail.com>
2013-05-29 03:42:02 +0400

    i could add dispatch and great monads and all other cool stuff...only igd=fg i didnt want to sledvz zl

--- 7/No1.hs
+++ 7/No1.hs
@@ -1,106 +1,105 @@
-open System.IO
-open System.Collections.Generic
-open System.Text
-open System
+import qualified Data.Map as Map  
+import Data.Char
+import Data.Maybe
+import System.IO
+import Control.Monad.Error
+import System.Environment  
+import System.Directory 
 
-let addNewRecord name number (currentData : Dictionary<string, string>) =
-    if (currentData.ContainsKey number) then 
-        printf "Невозможно добавить %s " name
-        printfn "В книге уже есть человек с номером %s" number
-    else 
-        currentData.Add(number, name)
+data ReadError = BadFile | OtherError String  
 
-let writeToFile path (str : string) =
-    try
-        use file = File.CreateText path
-        file.WriteLine str
-    with
-    | exn -> printfn "Ошибка! %s" exn.Message
+instance Error ReadError where
+  noMsg    = OtherError "A read Error!"
+  strMsg s = OtherError s
 
-let saveData path (currentData : Dictionary<string, string>) =
-    let data = new StringBuilder()
-    let append (str : string) = data.Append str |> ignore
-    for pair in currentData do
-        append(pair.Value + "\n" + pair.Key + "\n")   
-    writeToFile path (data.ToString())
+instance Show ReadError where
+  show BadFile = "Incorrect input"
+  show (OtherError msg) = msg
 
-let readData path (currentData : Dictionary<string, string>) = 
-    try
-         use reader = File.OpenText path
-         let rec readLines() =
-            let line1 = reader.ReadLine()
-            let line2 = reader.ReadLine()
-            match line1 with
-            | null | ""  -> ()
-            | _ ->
-                addNewRecord line1 line2 currentData
-                readLines() 
-         readLines()
-    with
-    | exn -> printfn "Ошибка! %s" exn.Message
+--type MyMonad = Either ReadError
 
-let findNameByNumber number (currentData : Dictionary<string, string>) =
-    if currentData.ContainsKey number then
-        printfn "%s" currentData.[number]
-    else
-        printfn "В телефонной книге нет человека с таким номером"
+readin :: String -> IO (Map.Map String Integer)
+readin src = do
+    let p = map words (lines src)
+    if all (\x->length x == 2) p
+       then if all (\ (a:b:_) -> (reads b::[(Integer, String)]) /= []) p
+          then do let pairs   = map (split.words) (lines src)
+                  return (foldr insert Map.empty pairs)
+          else do putStrLn "Not all are integers"
+                  return Map.empty
+       else do putStrLn "Incorrect format"
+               return Map.empty 
+    where
+      insert (s, g) = Map.insert s g
+      split [name,number] = (name, read number :: Integer)
 
-let findNumberByName name (currentData : Dictionary<string, string>) =
-    if currentData.ContainsValue(name) then
-        for pair in currentData do
-            if pair.Value.Equals name then
-                printfn "%s" pair.Key
-    else
-        printfn "В телефонной книге нет человека с таким именем"
+toString :: Map.Map String Integer -> String
+toString k = toString' (Map.toList k)
 
-let isEmptyString string = 
-    string = ""
+toString' :: [(String, Integer)] -> String
+toString' [] = ""
+toString' ((a,b):xs) = a ++ " " ++ show b ++ "\n" ++ (toString' xs)
 
-let rec checkEmpty() =
-    let string = Console.ReadLine()
-    if isEmptyString string then 
-        printfn "%s" "Вы ничего не ввели! Пожалуйста, введите строку"
-        checkEmpty()
-    else string
-
-let rec startProcess data =
-    printf "\nВведите цифру от 0 до 5:
-0 - выйти
-1 - добавить запись (имя и телефон)
-2 - найти телефон по имени
-3 - найти имя по телефону
-4 - сохранить текущие данные в файл
-5 - считать данные из файла\n\n"
-
-    let key = Console.ReadLine()
-    Console.Clear()
-    match key with
-    | "0" -> exit 0
-    | "1" ->
-         printfn "%s" "\nВведите имя"
-         let name = checkEmpty()
-         printfn "%s" "\nВведите номер телефона"
-         let number = checkEmpty()
-         addNewRecord name number data
-         startProcess data
-    | "2" ->
-        printfn "%s" "\nВведите имя человека, номер которого вы хотите найти"
-        findNumberByName(Console.ReadLine()) data
-        startProcess data
-    | "3" ->
-        printfn "%s" "\nВведите номер телефона, чтобы узнать имя владельца"
-        findNameByNumber(Console.ReadLine()) data
-        startProcess data
-    | "4" -> 
-        saveData "phoneBook.txt" data
-        startProcess data
-    | "5" -> 
-        readData "phoneBook.txt" data
-        startProcess data   
-    | _ -> 
-        printfn "%s" "\nОшибка ввода. Попробуйте ещё раз"
-        startProcess data
-
-let currentData = new Dictionary<string, string>()
-startProcess currentData
- 
+main = do
+    hSetBuffering stdin LineBuffering
+    doLoop Map.empty
+    
+--reportResult :: MyMonad IO() -> IO ()
+--reportResult (Right smth) = putStrLn ("Computations ended correctly")
+--reportResult (Left e) = putStrLn ("Computations failed with error: " ++ (show e))
+    
+doLoop :: Map.Map String Integer -> IO()     
+doLoop book = do    
+    putStr "Enter:\n 0 - exit\n 1 - add\n 2 - lookup name\n 3 - lookup number\n 4 - save to file\n 5 - read from\n 6 - show\n"
+    command <- getLine
+    case command of
+        "0"-> return ()
+        "1" -> do 
+                  putStrLn "Reading name "
+                  nam <- getLine
+                  putStrLn "Reading number "
+                  num <- getLine    
+                  if (reads num :: [(Integer,String)]) /= []
+                    then doLoop (Map.insert nam (read num :: Integer) book)
+                    else do putStrLn "Failed to read the number, care to try again?"
+                            doLoop book
+        "2" -> do 
+                  putStrLn "Reading name"
+                  nam <- getLine
+                  putStr "Number:\n"
+                  putStrLn $ show $ Map.findWithDefault 0 nam book
+                  doLoop book
+        "3" -> do
+                  putStrLn "Reading number "
+                  num' <- getLine
+                  if (reads num' :: [(Integer,String)]) /= []
+                      then do putStr $ "Name(s):\n"
+                              putStrLn $ show.unwords.(map fst). Map.toList $ Map.filter (==  (read num' :: Integer))  book                              
+                      else putStrLn "Failed to read the number, care to try again?"
+                  doLoop book
+        "5" -> do
+                  putStr "Enter path\n"
+                  x <- getLine
+                  fileExists <- doesFileExist x  
+                  if fileExists  
+                    then do src <- readFile x
+                            book' <- readin src  
+                            doLoop book'
+                    else do putStrLn "The file doesn't exist!"  
+                            doLoop book
+        "4" -> do
+                  putStr "Enter name\n"
+                  x <- getLine
+                  writeFile x (toString book)
+                  doLoop book
+        "6" -> do
+                  putStrLn $ show book
+                  doLoop book
+        _  -> do 
+                  putStrLn "Error: No Such Command!"
+                  doLoop book
+        
+        
+        
+        
+        
